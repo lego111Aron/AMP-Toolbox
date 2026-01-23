fe08434844dd1bb928aded7b75866cfe42e74df8
Palfi Aron <h376388@stud.u-szeged.hu>
2026-01-23 16:54:42 +0100

    portupdate

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ def hello():
     return "<h1>Szia! Az AMP Python AppRunner mukodik!</h1><p>A toolkit alapja keszen all.</p>"
 
 if __name__ == "__main__":
-    # Az AMP-nek fontos, hogy a 0.0.0.0-as IP-n hallgasson
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=42401)
